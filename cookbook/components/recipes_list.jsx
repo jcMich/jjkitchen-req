870454df9925f4fa41bbed42069772b35bd9cfb4
Juan Manuel <jc.fie.umich@gmail.com>
2016-10-21 18:24:56 -0500

    recipe item key is now pk

--- cookbook/components/recipes_list.jsx
+++ cookbook/components/recipes_list.jsx
@@ -4,10 +4,9 @@ import { RecipeItem } from '../components/recipe_item';
 export const RecipesList = ({recipes}) => {
     const recipes_items = recipes.map((recipe)  => {
         return (
-            <RecipeItem recipe={ recipe } key={recipe.title}/>
+            <RecipeItem recipe={ recipe } key={recipe.pk}/>
         );
     });
-    
     return (
         <div className='card-columns'>
             { recipes_items }
